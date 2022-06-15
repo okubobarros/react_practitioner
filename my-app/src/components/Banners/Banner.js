b9d5472cc43407c13e0188d570951560e867f68e
Gustavo <guscooke14@gmail.com>
2022-06-15 11:07:35 -0300

    banner improved

--- my-app/src/components/Banners/Banner.js
+++ my-app/src/components/Banners/Banner.js
@@ -1,7 +1,19 @@
-
+import { Grid, Typography } from '@mui/material'
 
 export default function Banner() {
     return (
-        <h1>Banner **** Banner **** Banner</h1>
+        <Grid
+            container
+            direction='row'
+            justifyContent='center'
+            alignItems='center'
+            sx={{ minHeight: '50VH', bgcolor: '#007683' }}>
+            <Grid item sx={{ color: '#fff' }}>
+                <Typography variant='h2' fontSize='bold'>
+                    your banner!!!
+
+                </Typography>
+            </Grid>
+        </Grid>
     )
 }
