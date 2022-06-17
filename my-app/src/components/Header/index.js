8bd46fc88670c2d3979ded64f54cde4a1c267c77
Gustavo <guscooke14@gmail.com>
2022-06-17 14:55:57 -0300

    fix

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -2,22 +2,25 @@ import { Grid, Button } from '@mui/material'
 import { useEffect, useState } from 'react'
 
 export default function Banner() {
-    // Destructuring Es6, essa é a base do useState, a syntaxe será essa always - o que pode mudar estará dentro do que vc espera dentro do parametro, no caso abaixo uma String
+    //   Essa é a base do useState, a syntaxe será essa always - 
+    // o que pode mudar estará dentro do que vc espera dentro do parametro, no caso abaixo uma String
     // para ilustrar o exemplo, mas poderia ser uma string vazia, uma array []...
+
     const [text, setText] = useState('');
+
+    // nao estamos usando mas, para vc ver que conforme a necessidade de mudar o estado vc cria um novo useState, tentar dividir a aplicação visualmente
     const [number, setNumber] = useState();
 
     // os eventos que são acionados, passamos uma arrow func para simplificar, no caso não ha necessidade de return ou {}
     const clickHandler = () => setText('');
-    const goBack = () => setText('');
 
-    // const increase = () => setNumber(+1)
 
+    // podemos escrever assim tb, não ha certo ou errado
     // function goBack() {
     //     setText("hi")
     // }
 
-    // Esse hook para um segundo momento
+    // Esse hook para um segundo momento...sideeffects
     useEffect(() => {
 
     }, [])
@@ -31,13 +34,15 @@ export default function Banner() {
             sx={{ minHeight: '50VH', bgcolor: '#238' }}>
             <Grid item sx={{ color: '#fff' }}>
                 <p>React Hooks, useState, bem simplificado para compreensão!</p>
-                <h1>where are you? {text || 'change it'}</h1>
+                <h1>Test React State, write anything: {text || 'change me'}</h1>
+
                 {/* onClick = evebto padrao para o react é uma funcao para chamar no click mesmo */}
                 {/* <Button onClick={clickHandler} text={'#FFF'} variant={'contained'} >
                     change
                 </Button> */}
-                <Button variant={'outlined'} onClick={goBack}>
-                    Return
+
+                <Button variant={'contained'} onClick={clickHandler}>
+                    go Back
                 </Button>
                 <input onChange={(e) => setText(e.target.value)}></input>
             </Grid>
